5c21cec60a3ff45e05622553b6e36ada292bf6e1
Rian Prates Da Silva Oliveira <165703830+R-Prates-16@users.noreply.github.com>
2024-10-04 16:10:01 -0300

    "Exercícios Finalizados"

---
+++ SQL - Dia 1 (Exercícios).sql
@@ -0,0 +1,59 @@
+--1-A)
+--A quantidade de produtos continuou o mesmo 
+--SELECT TOP (100) PERCENT ProductName     
+--FROM DimProduct;
+
+--1-B)
+--A quantidade de clientes diminuiu
+--SELECT FirstName, FirstName 
+--FROM DimCustomer;
+
+--2- A)
+--Todos os requisitos pedidos foram selecionados
+--SELECT CustomerKey, FirstName, EmailAddress, BirthDate
+--FROM DimCustomer
+
+--2-B)
+--Todos os nomes das colunas foram trocados
+--SELECT
+--CustomerKey AS 'ID Cliente', 
+--FirstName AS 'Primeiro Nome',
+--EmailAddress AS 'Email do Cliente',
+--BirthDate AS 'Data de Aniversário'
+--FROM DimCustomer
+
+--3-A)
+--Selecionei as 100 primeiras linhas da tebala 
+--SELECT TOP(100) *
+--FROM DimCustomer
+
+--3-B)
+--Selecionei 20% da história da tebale
+--SELECT TOP(20) PERCENT * 
+--FROM DimCustomer
+
+--3-C)
+--Selecionei as 100 primeiras linhas das tabelas da respectivas colunas abaixo
+--SELECT TOP(100) FirstName, EmailAddress, BirthDate
+--FROM DimCustomer
+
+--3-D)
+-- Renomiei as colunas indicadas abaixo
+--SELECT
+--FirstName AS 'Primeiro Nome',
+--EmailAddress AS 'Email do Cliente',
+--BirthDate AS 'Data de Aniversário'
+--FROM DimCustomer
+
+--4) Como não foi possível achar a coluna fornecedor, utilizei a coluna 'ClassName' como exemplo
+--SELECT
+--ClassName AS Fornecedor
+--FROM DimProduct
+
+--5) 
+--Significa que não vendeu um produto
+--SELECT *
+--FROM DimProduct
+
+--SELECT *
+--FROM FactSales
