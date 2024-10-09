f77244e1ebc64d8aa321bd2923bd9cd55a2c1961
Rian Prates Da Silva Oliveira <165703830+R-Prates-16@users.noreply.github.com>
2024-10-09 14:13:26 -0300

    SQL Dia 1

--- SQL - Dia 1 (Exercícios).sql
+++ SQL - Dia 1 (Exercícios).sql
@@ -1,74 +1,74 @@
 --1-A)
 --A quantidade de produtos continuou o mesmo 
---SELECT TOP (100) PERCENT 
+SELECT TOP (100) PERCENT 
 ProductName   
   
---FROM DimProduct;
+FROM DimProduct;
 
 --1-B)
 --A quantidade de clientes diminuiu
---SELECT 
+SELECT 
   FirstName
   
---FROM DimCustomer;
+FROM DimCustomer;
 
 --2- A)
 --Todos os requisitos pedidos foram selecionados
---SELECT 
+SELECT 
   CustomerKey, FirstName, EmailAddress, BirthDate
   
---FROM DimCustomer
+FROM DimCustomer
 
 --2-B)
 --Todos os nomes das colunas foram trocados
---SELECT
---CustomerKey AS 'ID Cliente', 
---FirstName AS 'Primeiro Nome',
---EmailAddress AS 'Email do Cliente',
---BirthDate AS 'Data de Aniversário'
+SELECT
+CustomerKey AS 'ID Cliente', 
+FirstName AS 'Primeiro Nome',
+EmailAddress AS 'Email do Cliente',
+BirthDate AS 'Data de Aniversário'
   
---FROM DimCustomer
+FROM DimCustomer
 
 --3-A)
 --Selecionei as 100 primeiras linhas da tabela 
---SELECT TOP(100) *
+SELECT TOP(100) *
   
---FROM DimCustomer
+FROM DimCustomer
 
 --3-B)
 --Selecionei 20% da história da tabela
---SELECT TOP(20) PERCENT * 
+SELECT TOP(20) PERCENT * 
   
---FROM DimCustomer
+FROM DimCustomer
 
 --3-C)
 --Selecionei as 100 primeiras linhas das tabelas da respectivas colunas abaixo
---SELECT TOP(100)
+SELECT TOP(100)
 FirstName, EmailAddress, BirthDate
 
---FROM DimCustomer
+FROM DimCustomer
 
 --3-D)
 -- Renomiei as colunas indicadas abaixo
---SELECT
---FirstName AS 'Primeiro Nome',
---EmailAddress AS 'Email do Cliente',
---BirthDate AS 'Data de Aniversário'
+SELECT
+FirstName AS 'Primeiro Nome',
+EmailAddress AS 'Email do Cliente',
+BirthDate AS 'Data de Aniversário'
 
---FROM DimCustomer
+FROM DimCustomer
 
 --4) Como não foi possível achar a coluna fornecedor, utilizei a coluna 'Manufacturer' como exemplo
---SELECT
---Manufacturer AS Fornecedor
+SELECT
+Manufacturer AS Fornecedor
 
---FROM DimProduct
+FROM DimProduct
 
 --5) 
 --Significa que não vendeu um produto
---SELECT *
+SELECT *
 
---FROM DimProduct
+FROM DimProduct
 
---SELECT *
+SELECT *
 
---FROM FactSales
+FROM FactSales
