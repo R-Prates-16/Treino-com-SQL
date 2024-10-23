43e2b56632fe86458a160561a9bd8681ba627c84
Rian Prates Da Silva Oliveira <165703830+R-Prates-16@users.noreply.github.com>
2024-10-23 15:50:23 -0300

    Criação de loja

--- LojaDB- (Exercícios).sql
+++ LojaDB- (Exercícios).sql
@@ -2,32 +2,32 @@
 -- Criando o banco de dados chamado LojaDB
 CREATE DATABASE LojaDB;
 
--- Usando o banco de dados LojaDB para as prÛximas operaÁıes
+-- Usando o banco de dados LojaDB para as pr√≥ximas opera√ß√µes
 USE LojaDB;
 -- Criando a tabela Clientes que armazena os dados dos clientes
 CREATE TABLE Clientes (
-    ClienteID INT PRIMARY KEY,  -- ClienteID È a chave prim·ria, ou seja, identifica unicamente cada cliente
-    NomeCliente VARCHAR(100),   -- Nome do cliente, armazenado como uma string de atÈ 100 caracteres
-    Cidade VARCHAR(50)          -- Cidade onde o cliente mora, armazenado como uma string de atÈ 50 caracteres
+    ClienteID INT PRIMARY KEY,  -- ClienteID √© a chave prim√°ria, ou seja, identifica unicamente cada cliente
+    NomeCliente VARCHAR(100),   -- Nome do cliente, armazenado como uma string de at√© 100 caracteres
+    Cidade VARCHAR(50)          -- Cidade onde o cliente mora, armazenado como uma string de at√© 50 caracteres
 );
 
 -- Criando a tabela Pedidos que armazena os pedidos feitos pelos clientes
 USE LojaDB;
 CREATE TABLE Pedidos (
-    PedidoID INT PRIMARY KEY,   -- PedidoID È a chave prim·ria, identificando unicamente cada pedido
-    ClienteID INT,              -- ClienteID È uma chave estrangeira, associando cada pedido a um cliente
+    PedidoID INT PRIMARY KEY,   -- PedidoID √© a chave prim√°ria, identificando unicamente cada pedido
+    ClienteID INT,              -- ClienteID √© uma chave estrangeira, associando cada pedido a um cliente
     DataPedido DATE,            -- Data do pedido, armazenada como um valor de data
-    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)  -- ClienteID È uma chave estrangeira que referencia a chave prim·ria da tabela Clientes
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)  -- ClienteID √© uma chave estrangeira que referencia a chave prim√°ria da tabela Clientes
 );
 
 
--- Criando a tabela Produtos que armazena os dados dos produtos disponÌveis
+-- Criando a tabela Produtos que armazena os dados dos produtos dispon√≠veis
 USE LojaDB;
 CREATE TABLE Produtos (
-    ProdutoID INT PRIMARY KEY,  -- ProdutoID È a chave prim·ria, identificando unicamente cada produto
+    ProdutoID INT PRIMARY KEY,  -- ProdutoID √© a chave prim√°ria, identificando unicamente cada produto
     NomeProduto VARCHAR(100),   -- Nome do produto
-    CategoriaID INT,            -- CategoriaID È uma chave estrangeira que referencia a tabela Categorias
-    Preco DECIMAL(10, 2)        -- PreÁo do produto, com atÈ 10 dÌgitos no total e 2 casas decimais
+    CategoriaID INT,            -- CategoriaID √© uma chave estrangeira que referencia a tabela Categorias
+    Preco DECIMAL(10, 2)        -- Pre√ßo do produto, com at√© 10 d√≠gitos no total e 2 casas decimais
 );
 
 
@@ -35,15 +35,15 @@ CREATE TABLE Produtos (
 -- Criando a tabela Categorias que armazena as categorias dos produtos
 USE LojaDB;
 CREATE TABLE Categorias (
-    CategoriaID INT PRIMARY KEY,  -- CategoriaID È a chave prim·ria, identificando unicamente cada categoria
+    CategoriaID INT PRIMARY KEY,  -- CategoriaID √© a chave prim√°ria, identificando unicamente cada categoria
     NomeCategoria VARCHAR(100)    -- Nome da categoria
 );
 
 
 -- Inserindo clientes na tabela Clientes
 INSERT INTO Clientes (ClienteID, NomeCliente, Cidade) VALUES
-(1, 'Maria Silva', 'S„o Paulo'),  -- Cliente com ID 1
-(2, 'Jo„o Santos', 'Rio de Janeiro'),  -- Cliente com ID 2
+(1, 'Maria Silva', 'S√£o Paulo'),  -- Cliente com ID 1
+(2, 'Jo√£o Santos', 'Rio de Janeiro'),  -- Cliente com ID 2
 (3, 'Ana Costa', 'Belo Horizonte');  -- Cliente com ID 3
 
 
@@ -51,23 +51,23 @@ INSERT INTO Clientes (ClienteID, NomeCliente, Cidade) VALUES
 -- Inserindo pedidos na tabela Pedidos
 INSERT INTO Pedidos (PedidoID, ClienteID, DataPedido) VALUES
 (1, 1, '2024-01-15'),  -- Pedido ID 1 feito pelo cliente com ID 1 (Maria Silva)
-(2, 2, '2024-01-20'),  -- Pedido ID 2 feito pelo cliente com ID 2 (Jo„o Santos)
+(2, 2, '2024-01-20'),  -- Pedido ID 2 feito pelo cliente com ID 2 (Jo√£o Santos)
 (3, 1, '2024-02-10');  -- Pedido ID 3 feito pelo cliente com ID 1 (Maria Silva)
 
 
 
 -- Inserindo categorias na tabela Categorias
 INSERT INTO Categorias (CategoriaID, NomeCategoria) VALUES
-(1, 'EletrÙnicos'),  -- Categoria com ID 1
-(2, 'Vestu·rio');    -- Categoria com ID 2
+(1, 'Eletr√¥nicos'),  -- Categoria com ID 1
+(2, 'Vestu√°rio');    -- Categoria com ID 2
 
 
 
 -- Inserindo produtos na tabela Produtos
 INSERT INTO Produtos (ProdutoID, NomeProduto, CategoriaID, Preco) VALUES
-(1, 'Celular', 1, 1500.00),     -- Produto Celular, pertence ‡ categoria EletrÙnicos
-(2, 'Televis„o', 1, 3000.00),   -- Produto Televis„o, pertence ‡ categoria EletrÙnicos
-(3, 'Camiseta', 2, 50.00);      -- Produto Camiseta, pertence ‡ categoria Vestu·rio
+(1, 'Celular', 1, 1500.00),     -- Produto Celular, pertence √† categoria Eletr√¥nicos
+(2, 'Televis√£o', 1, 3000.00),   -- Produto Televis√£o, pertence √† categoria Eletr√¥nicos
+(3, 'Camiseta', 2, 50.00);      -- Produto Camiseta, pertence √† categoria Vestu√°rio
 
 
 
@@ -120,6 +120,7 @@ FROM dbo.Pedidos
 INNER JOIN dbo.Clientes
 ON dbo.Clientes.ClienteID = dbo.Pedidos.ClienteID
 
+    
 
 --4)
 SELECT
@@ -151,7 +152,7 @@ GROUP BY dbo.Clientes.NomeCliente, dbo.Clientes.Cidade
 -- Selecionando o banco de dados para uso
 USE LojaDB
 CREATE TABLE DetalhesPedidos (
-    DetalheID INT PRIMARY KEY IDENTITY(1,1),  -- Identificador ˙nico para cada linha
+    DetalheID INT PRIMARY KEY IDENTITY(1,1),  -- Identificador √∫nico para cada linha
     PedidoID INT,                             -- Relaciona com a tabela Pedidos
     ProdutoID INT,                            -- Relaciona com a tabela Produtos
     Quantidade INT,                           -- Quantidade de produtos no pedido
@@ -163,10 +164,10 @@ CREATE TABLE DetalhesPedidos (
 -- Inserindo detalhes dos pedidos
 INSERT INTO DetalhesPedidos (PedidoID, ProdutoID, Quantidade)
 VALUES
-(1, 1, 2),  -- Pedido 1 contÈm 2 unidades do Produto 1
-(1, 2, 1),  -- Pedido 1 contÈm 1 unidade do Produto 2
-(2, 3, 5),  -- Pedido 2 contÈm 5 unidades do Produto 3
-(3, 2, 2);  -- Pedido 3 contÈm 2 unidades do Produto 2
+(1, 1, 2),  -- Pedido 1 cont√©m 2 unidades do Produto 1
+(1, 2, 1),  -- Pedido 1 cont√©m 1 unidade do Produto 2
+(2, 3, 5),  -- Pedido 2 cont√©m 5 unidades do Produto 3
+(3, 2, 2);  -- Pedido 3 cont√©m 2 unidades do Produto 2
 
 
 SELECT 
@@ -203,6 +204,7 @@ ON dbo.DetalhesPedidos.PedidoID = dbo.Pedidos.PedidoID
 
 GROUP BY dbo.Clientes.NomeCliente
 
+    
 
 --7)
 SELECT
@@ -221,7 +223,7 @@ dbo.Produtos.NomeProduto
 --8)
 SELECT
 YEAR(Pedidos.DataPedido) AS 'ANO',
-MONTH(Pedidos.DataPedido) AS 'M S',
+MONTH(Pedidos.DataPedido) AS 'M√äS',
 COUNT(Pedidos.PedidoID) AS 'Pedidos'
 
 FROM dbo.Pedidos
@@ -266,7 +268,6 @@ SET Total = (
     INNER JOIN Produtos ON DetalhesPedidos.ProdutoID = Produtos.ProdutoID
     WHERE DetalhesPedidos.PedidoID = Pedidos.PedidoID
 );
-
 
 
 SELECT
